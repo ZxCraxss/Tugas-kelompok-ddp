bbe89c278238fd7ad5fb104f7fc74ada2113edc9
Muhammad Rizki Ramadan <162446493+ZxCraxss@users.noreply.github.com>
2024-10-05 11:52:43 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+char papan[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+char pemain = 'X';
+int baris, kolom;
+bool seri = false;
 
+void papannya() {
+cout<<"    |     |     "<<endl;
+cout<<"  "<<papan[0][0]<<" |  " <<papan[0][1]<<"  |  "<<papan[0][2]<<endl;
+cout<<"____|_____|_____"<<endl;
+}
+
+int main(){
+papannya();
 return 0;
 }
